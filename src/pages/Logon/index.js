b0ad9029373af01512cb4a33d0f9171a50171714
Moisés S E Aguilar <moisesmagno@gmail.com>
2020-04-10 00:45:24 -0300

    Criado a tela de registro e lista de casos.

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import { FiLogIn } from "react-icons/fi";
+
+import "./styles.css";
 
 import logoImg from "../../assets/logo.svg";
 import heroesImg from "../../assets/heroes.png";
@@ -11,7 +16,14 @@ export default function Logon() {
         <form>
           <h1>Faça o seu Login</h1>
           <input type="text" placeholder="Digite o seu ID" />
-          <button type="submit">Entrar</button>
+          <button className="button" type="submit">
+            Entrar
+          </button>
+
+          <Link className="back-link" to="/register">
+            <FiLogIn size={16} color="#E02041" />
+            Não tenho cadastro
+          </Link>
         </form>
       </section>
 
