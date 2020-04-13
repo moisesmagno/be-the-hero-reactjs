ffcf80dd338c0797c34d839e4f3def9ef1441343
Moisés S E Aguilar <moisesmagno@gmail.com>
2020-04-12 23:44:43 -0300

    Criado a tela de nova incidencia e implementado a funcionalidade que cadastra uma nova ong.

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import { FiArrowLeft } from "react-icons/fi";
 
+import api from "../../services/api";
 import "./styles.css";
 
 import logoImg from "../../assets/logo.svg";
 
 export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [whatsapp, setwhatsapp] = useState("");
+  const [city, setCity] = useState("");
+  const [uf, setUf] = useState("");
+
+  async function handleRegister(e) {
+    e.preventDefault();
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      city,
+      uf,
+    };
+
+    try {
+      const response = await api.post("ongs", data);
+
+      alert(`Seu ID de acesso é: ${response.data.id}`);
+    } catch (err) {
+      alert(`Erro no cadastro, por favor tente mais tarde!`);
+    }
+  }
+
   return (
     <div className="register-container">
       <div className="content">
@@ -15,7 +42,7 @@ export default function Register() {
           <img src={logoImg} alt="Be the hero" />
           <h1>Cadastro</h1>
           <p>
-            Faça seu cadastr, entre na plataforma e ajude pessoas a encontrarem
+            Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem
             os casos da sua ONG.
           </p>
           <Link className="back-link" to="/">
@@ -24,14 +51,40 @@ export default function Register() {
           </Link>
         </section>
 
-        <form>
-          <input type="text" placeholder="Nome da ONG" />
-          <input type="email" placeholder="E-mail" />
-          <input type="text" placeholder="Whatsapp" />
+        <form onSubmit={handleRegister}>
+          <input
+            type="text"
+            placeholder="Nome da ONG"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="E-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Whatsapp"
+            value={whatsapp}
+            onChange={(e) => setwhatsapp(e.target.value)}
+          />
 
           <div className="input-group">
-            <input type="text" placeholder="Cidade" />
-            <input type="text" placeholder="UF" style={{ width: 80 }} />
+            <input
+              type="text"
+              placeholder="Cidade"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="UF"
+              style={{ width: 80 }}
+              value={uf}
+              onChange={(e) => setUf(e.target.value)}
+            />
           </div>
 
           <button className="button" type="submit">
